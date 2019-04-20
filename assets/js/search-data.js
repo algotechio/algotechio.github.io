31f9768ece3d659225cac5ce3ab83bf11079bac5
boris <boris@algotech.io>
2019-04-20 22:35:54 +0800

    initial commit

---
+++ assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Edge Case: Many Categories",
+        "excerpt":"This post has many categories. ","categories": ["aciform","antiquarianism","arrangement","asmodeus","broder","buying","championship","chastening","disinclination","disinfection"],
+        "tags": ["categories","edge case"],
+        "url": "http://0.0.0.0:4000/aciform/antiquarianism/arrangement/asmodeus/broder/buying/championship/chastening/disinclination/disinfection/edge-case-many-categories/"
+      }]
