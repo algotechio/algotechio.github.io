15ac2a1638a2a8417b83c3cfb83b5a9eb5f4ca86
boris <boris@algotech.io>
2019-04-22 15:01:27 +0800

    changes

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,6 +1,1 @@
-var store = [{
-        "title": "Future Post",
-        "excerpt":"First post to be written. ","categories": ["to be defined"],
-        "tags": ["no tags yet"],
-        "url": "https://blog.algotech.io/to%20be%20defined/no-posts-yet/"
-      }]
+var store = []
